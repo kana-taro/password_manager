6fd672f007f704f4837f945318057d08b1f009a9
kana <ao.kmt@outlook.com>
2026-03-21 19:21:59 +0900

    パスワード保存用のファイル作成と保存処理の実装

--- passmanager.sh
+++ passmanager.sh
@@ -3,4 +3,5 @@ echo "パスワードマネージャーへようこそ！"
 read -p "サービス名を入力してください：" service
 read -p "ユーザー名を入力してください：" user
 read -p "パスワードを入力してください：" pass
+echo "$service:$user:$pass" >> password.txt
 echo "Thank you!"
